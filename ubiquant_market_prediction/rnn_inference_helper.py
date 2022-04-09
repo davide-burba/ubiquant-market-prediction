e0c1e60f4f054af6e04f0134d340015ab22db679
Davide <davide.burba@hotmail.com>
2022-04-09 14:56:56 +0200

    Update rnn inference helper

--- ubiquant_market_prediction/rnn_inference_helper.py
+++ ubiquant_market_prediction/rnn_inference_helper.py
@@ -10,6 +10,7 @@ def run_inference_rnn(
     hidden_state_helper,
     keep_investment_id=False,
     update_state_during_inference=True,
+    check_last_timestep=False,
 ):
     test_df = test_df.fillna(0)
     (
@@ -17,10 +18,16 @@ def run_inference_rnn(
         test_investment_ids,
         test_timesteps,
     ) = _build_tensor_features(test_df, keep_investment_id)
+    
+    if check_last_timestep:
+        last_timestep = min(test_timesteps) - 1
+    else:
+        last_timestep = None
 
     y_pred = _build_prediction_tensor(
         test_features,
         test_investment_ids,
+        last_timestep,
         model,
         preprocessor,
         hidden_state_helper,
@@ -34,7 +41,8 @@ def run_inference_rnn(
 
 
 def _build_tensor_features(test_df, keep_investment_id):
-    test_df["time_id"] = test_df.row_id.str.split("_", expand=True)[0].astype(int)
+    if "time_id" not in test_df.columns:
+        test_df["time_id"] = test_df.row_id.str.split("_", expand=True)[0].astype(int)
     test_investment_ids = np.array(sorted(test_df.investment_id.unique()))
     test_timesteps = np.array(sorted(test_df.time_id.unique()))
 
@@ -48,17 +56,18 @@ def _build_tensor_features(test_df, keep_investment_id):
 def _build_prediction_tensor(
     test_features,
     test_investment_ids,
+    last_timestep,
     model,
     preprocessor,
     hidden_state_helper,
     update_state_during_inference,
 ):
-
+    
     x_test = preprocessor.run_inference(test_features)
-    h_state_prev = hidden_state_helper.get_hstate(test_investment_ids)
+    h_state_prev = hidden_state_helper.get_hstate(test_investment_ids,last_timestep)
     y_pred, h_state = model._predict(x_test, h_state=h_state_prev)
     if update_state_during_inference:
-        hidden_state_helper.update_hstate(h_state, test_investment_ids)
+        hidden_state_helper.update_hstate(h_state, test_investment_ids,last_timestep+1)
     return y_pred
 
 
@@ -92,33 +101,43 @@ def _df_to_tensor(df, investment_ids, timesteps, keep_investment_id):
 
 class HiddenStateHelper:
     def __init__(
-        self, h_state, investment_ids, initialize_inv_id=True, h_state_default=None
+        self, h_state, investment_ids, initialize_inv_id=True, h_state_default=None,last_timestep=0,
     ):
 
         self.d1, _, self.d3 = h_state[0].shape
         self._h1 = {}
         self._h2 = {}
+        self._last_timestep = {}
+        
         self.h_state_default = h_state_default
         if initialize_inv_id:
-            self.update_hstate(h_state, investment_ids)
+            self.update_hstate(h_state, investment_ids,last_timestep)
 
-    def update_hstate(self, h_state, investment_ids):
+    def update_hstate(self, h_state, investment_ids,last_timestep):
         h1, h2 = h_state
         for i, inv_id in enumerate(investment_ids):
             self._h1[inv_id] = h1[:, i : i + 1, :]
             self._h2[inv_id] = h2[:, i : i + 1, :]
+            self._last_timestep[inv_id] = last_timestep
 
-    def get_hstate(self, investment_ids):
+    def get_hstate(self, investment_ids, last_timestep=None):
         h1 = []
         h2 = []
         for inv_id in investment_ids:
+            
+            get_default = True
             if inv_id in self._h1:
-                h1.append(self._h1[inv_id])
-                h2.append(self._h2[inv_id])
-            else:
+                if last_timestep is None or self._last_timestep[inv_id] == last_timestep:
+                    get_default = False
+                    
+            if get_default:
                 _h1, _h2 = self.get_default_state()
                 h1.append(_h1)
                 h2.append(_h2)
+            else:
+                h1.append(self._h1[inv_id])
+                h2.append(self._h2[inv_id])
+                
         h1 = torch.cat(h1, dim=1)
         h2 = torch.cat(h2, dim=1)
         return (h1, h2)
@@ -133,3 +152,5 @@ class HiddenStateHelper:
 
     def _build_zero_state(self):
         return torch.zeros(self.d1, 1, self.d3)
+
+
